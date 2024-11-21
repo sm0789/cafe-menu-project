51c8e695078df7fa80de91f65b333eb141450e21
Sanika <manjreka@purdue.edu>
2024-11-21 11:35:20 -0500

    added location page and linked other pages to it, also added search method for locations

---
+++ location.js
@@ -0,0 +1,14 @@
+const locationSearch = document.getElementById("location-search");
+locationSearch.addEventListener("keyup", (e) => {
+  let currentValue = e.target.value.toLowerCase();
+  let locations = document.querySelectorAll("h2.title"); //get all the locations since they are h2 elements and have a class called title
+  locations.forEach((location) => {
+    if (location.textContent.toLowerCase().includes(currentValue)) {
+      //show location card
+      location.parentNode.parentNode.style.display = "block";
+    } else {
+      //otherwise hide location card
+      location.parentNode.parentNode.style.display = "none";
+    }
+  });
+});
